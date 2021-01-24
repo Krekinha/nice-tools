75b2d3e4291b8b341c6e4e32720eae6b470f43af
Dionison <krekmg@gmail.com>
2021-01-24 20:15:17 -0300

    primeiro commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <h1 className="className">Tools</h1>
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  );
+}
